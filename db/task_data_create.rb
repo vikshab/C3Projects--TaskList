7bd4ec11bde1111abf5348c10d672aa66f0da95a
Ashley Watkins <AWatkins31@gmail.com>
2015-06-10 16:58:44 -0700

    Adds comment

--- db/task_data_create.rb
+++ db/task_data_create.rb
@@ -1,6 +1,9 @@
+# this file is not called anywhere, and it should instead
+# be run as part of the initial setup.
+
 require "sqlite3"
 
-db = SQLite3::Database.new "task_data.db"
+db = SQLite3::Database.new "db/task_data.db"
 
 db.execute "CREATE TABLE tasks
   (id INTEGER PRIMARY KEY, task TEXT NOT NULL, description TEXT, completed_on TEXT);"
