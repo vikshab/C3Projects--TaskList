3040e92e8e951813e1121a562fff91b2250e0c60
jnf <jeremy.flores@gmail.com>
2015-06-10 12:57:46 -0700

    Add basic project setup and requirements.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+ruby   '2.2.2'
+
+gem 'sqlite3', '~> 1.3.10'
---
+++ lib/database.rb
@@ -0,0 +1,34 @@
+require 'sqlite3'
+
+module TaskList
+  class Database
+    attr_reader :database_name
+
+    def initialize(database_name)
+      @database_name = "db/#{ database_name }"
+    end
+
+    def your_custom_query_here(*args)
+      # santitize/validate your arguments
+
+      # prepare your statement
+
+      # call `query!` to interact with the database
+
+      # determine what should be returned
+    end
+
+    private
+
+    def query!(statement, *params)
+      db = SQLite3::Database.new database_name
+      db.execute statement, params
+    rescue SQLite3::Exception => error
+      # use this block to recover from an error
+      # consider giving the user a special message back
+      # inspect the `error` object for information about the error
+    ensure
+      db.close if db
+    end
+  end
+end
