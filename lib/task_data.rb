ca3660b40a209d14bc2d054e19b9ddd268ea184f
Ashley Watkins <AWatkins31@gmail.com>
2015-06-10 18:36:51 -0700

    Adds #add_task + and #display_list methods for breakfast (currently untested)

--- lib/task_data.rb
+++ lib/task_data.rb
@@ -1,8 +1,58 @@
-# require "database"
 require 'sqlite3'
+require_relative './database'
 
 module TaskList
-  class TaskData #< Database
+  class TaskData < Database
+  ####   attr_reader :database_name
+
+  ####   def initialize(database_name)
+  ####     @database_path = "db/#{ database_name }"
+  ####   end
+
+  def add_task(task, description)
+    query!("INSERT INTO tasks (task, description) VALUES (?, ?);", task, descriptions)
+  end
+
+  def display_list
+    query!("SELECT * FROM tasks;")
+  end
+
+  def your_custom_query_here(*args)
+    # santitize/validate your arguments
+    # prepare your statement
+    # call `query!` to interact with the database
+    # determine what should be returned
+  end
 
   end #ends class
 end # ends module
+
+
+
+  # class Database
+
+
+
+  #   def your_custom_query_here(*args)
+  #     # sanitize/validate your arguments
+
+  #     # prepare your statement
+
+  #     # call `query!` to interact with the database
+
+  #     # determine what should be returned
+  #   end
+
+  #   private
+
+  #   def query!(statement, *params)
+  #     db = SQLite3::Database.new(@database_path)
+  #     db.execute statement, params
+  #   rescue SQLite3::Exception => error
+  #     # use this block to recover from an error
+  #     # consider giving the user a special message back
+  #     # inspect the `error` object for information about the error
+  #   ensure
+  #     db.close if db
+  #   end
+  # end
